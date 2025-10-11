4dbdac46300912befc8a362c514da4bffc9a29ff
Corey Clements <co430015@ucf.edu>
2025-10-11 18:29:58 -0400

    FROG-66-LilypadFrogs
    Replaced bubbles with lilypad frogs, added logic for showing/hiding frogs and launching player onto islands

--- Froguelite/Assets/Scripts/Generation/Door.cs
+++ Froguelite/Assets/Scripts/Generation/Door.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Door : MonoBehaviour
@@ -19,15 +20,29 @@ public class Door : MonoBehaviour
 
     public DoorData doorData { get; private set; }
 
-    [SerializeField] private SpriteRenderer bubbleRenderer;
-    [SerializeField] private SpriteRenderer shadowRenderer;
+    private bool isTravelling = false;
+    private bool invertSpriteDirection = false;
+
+    [SerializeField] private SpriteRenderer frogRenderer;
+    [SerializeField] private SpriteMask frogMask;
+    [SerializeField] private ParticleSystem rippleParticles;
+
+    [SerializeField] private Sprite unlockedSpriteUp;
+    [SerializeField] private Sprite lockedSpriteUp;
+    [SerializeField] private Sprite unlockedSpriteDown;
+    [SerializeField] private Sprite lockedSpriteDown;
+    [SerializeField] private Sprite unlockedSpriteLeft;
+    [SerializeField] private Sprite lockedSpriteLeft;
+    [SerializeField] private Sprite unlockedSpriteRight;
+    [SerializeField] private Sprite lockedSpriteRight;
 
     // Floating animation variables
-    private Vector3 bubbleOriginalPosition;
+    private Vector3 frogRendShownPos;
+    private Vector3 frogRendHiddenPos;
     private float floatSpeed = 1f;
     private float floatRange = 0.3f;
-    private float shadowDistance = 1f;
     private float timeOffset;
+    private bool inShownPosition = true;
 
 
     #endregion
@@ -35,21 +50,11 @@ public class Door : MonoBehaviour
 
     #region MONOBEHAVIOUR
 
-
-    private void Start()
-    {
-        if (bubbleRenderer != null)
-        {
-            bubbleOriginalPosition = bubbleRenderer.transform.position;
-            timeOffset = Random.Range(0f, 2f * Mathf.PI); // Random starting phase for variety
-        }
-    }
-
     private void Update()
     {
-        if (bubbleRenderer != null)
+        if (frogMask != null)
         {
-            FloatBubble();
+            FloatFrog();
         }
     }
 
@@ -64,12 +69,22 @@ public class Door : MonoBehaviour
     public void InitializeDoorData(DoorData data)
     {
         doorData = data;
+
+        frogRendShownPos = frogRenderer.transform.localPosition;
+        frogRendHiddenPos = frogRendShownPos - new Vector3(0, 1, 0);
+        timeOffset = Random.Range(0f, 2f * Mathf.PI); // Random starting phase for variety
+
+        DoorManager.Instance.RegisterDoor(this);
+
+        UpdateDoorVisuals();
     }
 
     // Updates door's data with given new data
     public void UpdateDoorData(DoorData newData)
     {
         doorData = newData;
+
+        UpdateDoorVisuals();
     }
 
 
@@ -79,28 +94,108 @@ public class Door : MonoBehaviour
     #region SPAWNING AND VISUALS
 
 
-    // Spawns the door at a specific X, Y position in the room, based on doorData
-    public void SpawnDoor()
-    {
-        // TODO
-    }
-
     // Updates the door's visual representation based on its current state (open, closed, locked, etc.)
-    private void UpdateDoorVisuals()
+    public void UpdateDoorVisuals(bool animate = true)
     {
-        // TODO
+        if (doorData.isImpassable)
+        {
+            frogRenderer.enabled = false;
+            return;
+        }
+
+        // Set sprites to correct direction and locked / unlocked
+        if (doorData.isLocked)
+        {
+            switch (doorData.direction)
+            {
+                case DoorDirection.Up:
+                    frogRenderer.sprite = invertSpriteDirection ? lockedSpriteUp : lockedSpriteDown;
+                    break;
+                case DoorDirection.Down:
+                    frogRenderer.sprite = invertSpriteDirection ? lockedSpriteDown : lockedSpriteUp;
+                    break;
+                case DoorDirection.Left:
+                    frogRenderer.sprite = invertSpriteDirection ? lockedSpriteLeft : lockedSpriteRight;
+                    break;
+                case DoorDirection.Right:
+                    frogRenderer.sprite = invertSpriteDirection ? lockedSpriteRight : lockedSpriteLeft;
+                    break;
+            }
+        }
+        else
+        {
+            switch (doorData.direction)
+            {
+                case DoorDirection.Up:
+                    frogRenderer.sprite = invertSpriteDirection ? unlockedSpriteUp : unlockedSpriteDown;
+                    break;
+                case DoorDirection.Down:
+                    frogRenderer.sprite = invertSpriteDirection ? unlockedSpriteDown : unlockedSpriteUp;
+                    break;
+                case DoorDirection.Left:
+                    frogRenderer.sprite = invertSpriteDirection ? unlockedSpriteLeft : unlockedSpriteRight;
+                    break;
+                case DoorDirection.Right:
+                    frogRenderer.sprite = invertSpriteDirection ? unlockedSpriteRight : unlockedSpriteLeft;
+                    break;
+            }
+        }
+
+        // If we are travelling, don't need to worry about showing / hiding
+        if (isTravelling) return;
+
+        // Show or hide the frog accordingly
+        if (doorData.isOpen)
+        {
+            if (inShownPosition) return;
+
+            inShownPosition = true;
+            LeanTween.cancel(frogRenderer.gameObject);
+
+            frogRenderer.enabled = true;
+            rippleParticles.Play();
+
+            if (animate)
+            {
+                frogRenderer.transform.LeanMoveLocal(frogRendShownPos, 0.5f).setEaseOutSine();
+            }
+            else
+            {
+                frogRenderer.transform.localPosition = frogRendShownPos;
+            }
+        }
+        else
+        {
+            if (!inShownPosition) return;
+
+            inShownPosition = false;
+            LeanTween.cancel(frogRenderer.gameObject);
+            rippleParticles.Stop();
+
+            if (animate)
+            {
+                frogRenderer.transform.LeanMoveLocal(frogRendHiddenPos, 0.5f).setEaseInSine().setOnComplete(() =>
+                {
+                    frogRenderer.enabled = false;
+                });
+            }
+            else
+            {
+                frogRenderer.transform.localPosition = frogRendHiddenPos;
+                frogRenderer.enabled = false;
+            }
+        }
     }
 
-    // Makes the bubble renderer float around its original position
-    private void FloatBubble()
+    // Makes the frog renderer float around its original position in the water
+    private void FloatFrog()
     {
         // Create smooth floating motion using sine waves
         float x = Mathf.Sin((Time.time + timeOffset) * floatSpeed) * floatRange;
         float y = Mathf.Sin((Time.time + timeOffset) * floatSpeed * 1.3f) * floatRange * 0.7f;
 
         // Apply the floating offset as local position
-        bubbleRenderer.transform.localPosition = new Vector3(x, y, 0);
-        shadowRenderer.transform.localPosition = new Vector3(x, -shadowDistance, 0);
+        frogMask.transform.localPosition = new Vector3(x, y, 0);
     }
 
 
@@ -178,10 +273,22 @@ public class Door : MonoBehaviour
     {
         if (GameManager.Instance.currentPlayerState == GameManager.PlayerState.Exploring &&
             doorData.isOpen &&
-            !doorData.isLocked &&
             !doorData.isImpassable)
         {
+            if (doorData.isLocked)
+            {
+                if (InventoryManager.Instance.woodpeckers <= 0) return;
+
+                InventoryManager.Instance.RemoveWoodpeckers(1);
+                UnlockDoor();
+
+                PlayerAttack.Instance.StopTongueExtension(false);
+                return;
+            }
+
             GameManager.Instance.SetPlayerState(GameManager.PlayerState.InBubble);
+            isTravelling = true;
+            DoorManager.Instance.OnTravelStarted();
 
             // Make it so the player cannot move or attack, and suck the player into the bubble
             PlayerAttack.Instance.StopTongueExtension(true);
@@ -191,25 +298,78 @@ public class Door : MonoBehaviour
 
             PlayerMovement.Instance.onReachManualMoveTarget.AddListener(() =>
             {
-                // Make sure the player is exactly inside the bubble, and parent them to it so they move with it
-                PlayerMovement.Instance.transform.position = bubbleRenderer.transform.position;
-                PlayerMovement.Instance.transform.SetParent(bubbleRenderer.transform);
+                // Make sure the player is exactly inside the frog, and parent them to it so they move with it
+                PlayerMovement.Instance.transform.position = frogMask.transform.position;
+                PlayerMovement.Instance.transform.SetParent(frogMask.transform);
 
-                // Tween the bubble from current position (launch position) to other island (landing position)
-                transform.LeanMove(doorData.landingPosition, 2f).setEaseInOutSine().setOnComplete(() =>
+                invertSpriteDirection = true;
+                UpdateDoorVisuals();
+
+                // Tween the frog from current position (launch position) to other island (landing position)
+                transform.LeanMove(doorData.otherRoomLaunchPosition, 2f).setEaseInOutSine().setOnComplete(() =>
                 {
-                    // Unparent the player, re-enable movement and attacking, and end the travel process
+                    // Unparent the player, and move them to the landing position
                     PlayerMovement.Instance.transform.SetParent(null);
-                    PlayerMovement.Instance.EnableCollision();
-                    PlayerMovement.Instance.SetCanMove(true);
-                    PlayerAttack.Instance.SetCanAttack(true);
+                    StartCoroutine(LaunchPlayerToLanding());
 
-                    transform.position = doorData.launchPosition; // Reset the bubble position to the launch position
+                    invertSpriteDirection = false;
+                    UpdateDoorVisuals();
+
+                    transform.LeanMove(doorData.launchPosition, 2f).setEaseInOutSine().setOnComplete(() =>
+                    {
+                        isTravelling = false;
+                        UpdateDoorVisuals();
+                        DoorManager.Instance.OnTravelEnded();
+                    });
 
                     GameManager.Instance.SetPlayerState(GameManager.PlayerState.Exploring);
                 });
             });
         }
+    }
+
+
+    private IEnumerator LaunchPlayerToLanding(float duration = .5f)
+    {
+        Vector3 startPosition = PlayerMovement.Instance.transform.position;
+        Vector3 endPosition = doorData.landingPosition;
+        
+        // Calculate arc height based on distance for a natural arc
+        float distance = Vector3.Distance(startPosition, endPosition);
+        float arcHeight = distance * 0.5f; // Arc height is half the distance for a nice curve
+        
+        float elapsedTime = 0f;
+        
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            
+            // Use easing for smooth acceleration/deceleration
+            float easedT = Mathf.SmoothStep(0f, 1f, t);
+            
+            // Calculate the horizontal position (linear interpolation)
+            Vector3 horizontalPos = Vector3.Lerp(startPosition, endPosition, easedT);
+            
+            // Calculate the vertical arc using a parabola
+            // Peak of arc occurs at t = 0.5
+            float arcOffset = 4f * arcHeight * t * (1f - t);
+            
+            // Combine horizontal movement with vertical arc
+            Vector3 arcPosition = horizontalPos + Vector3.up * arcOffset;
+            
+            PlayerMovement.Instance.transform.position = arcPosition;
+            
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        // Ensure we end exactly at the landing position
+        PlayerMovement.Instance.transform.position = endPosition;
+        
+        // Re-enable player collision and movement after landing
+        PlayerMovement.Instance.EnableCollision();
+        PlayerMovement.Instance.SetCanMove(true);
+        PlayerAttack.Instance.SetCanAttack(true);
     }
 
 
@@ -235,6 +395,7 @@ public class DoorData
 
     public Vector3 launchPosition; // World position where the door hovers, and will "launch" from
     public Vector3 landingPosition; // World position where the door will land on the other island
+    public Vector3 otherRoomLaunchPosition; // World position in the other room where the door launches from
 
 
     #endregion
@@ -246,6 +407,7 @@ public class DoorData
     public DoorData(
         Vector3 launchPosition = default,
         Vector3 landingPosition = default,
+        Vector3 otherRoomLaunchPosition = default,
         Door.DoorDirection direction = Door.DoorDirection.Up,
         bool isImpassable = true,
         bool isLocked = false,
@@ -258,6 +420,7 @@ public class DoorData
         this.direction = direction;
         this.launchPosition = launchPosition;
         this.landingPosition = landingPosition;
+        this.otherRoomLaunchPosition = otherRoomLaunchPosition;
     }
 
 
